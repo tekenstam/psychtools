5c39db707eb86bf35ea72b74b1b35faac860a7f9
Todd Ekenstam <tekenstam@gmail.com>
2023-03-12 14:22:13 -0700

    Add plots for subject correlation per emotion to RS score

--- MovieRating/analyzeData.m
+++ MovieRating/analyzeData.m
@@ -3,149 +3,96 @@ clear all;
 close all;
 
 %load results from result files
-resultsFolder = 'results/';
+if exist('/Users/annikaekenstam/Dropbox/Stimuli/results/','dir')
+    resultsFolder = '/Users/annikaekenstam/Dropbox/Stimuli/results/';
+elseif exist('/Users/corelabuser/Dropbox/Stimuli/results/','dir')
+    resultsFolder = '/Users/corelabuser/Dropbox/Stimuli/results/';
+else
+    resultsFolder = 'results/';
+end
+
 resultList = dir([resultsFolder,'*.mat']);
-numResults = length(resultList);
-subjIDList = zeros(numResults, 1);
-for resultNum = 1:numResults;
+numSubjects = length(resultList);
+subjIDList = zeros(numSubjects,1);
+
+for resultNum = 1:numSubjects;
     results{resultNum}=load([resultsFolder,resultList(resultNum).name]);
     subjIDList(resultNum,1) = results{resultNum}.result.subID;
 end
-%subjIDList=subjIDList.';
 
-% The syntax for cell arrays is
-% emotionList = {'disgust','fear','happy','sad'};
-% curly brackets signify cell arrays
-% square brackets signify numerical, string, and character arrays
-% using ["disgust"...] seems to work but it is not consistent with other
-% syntax of cell arrays
-
-%set list of emotions to analyze from info in first result set
-%(they should all be the same)
+%set list of emotions to analyze from
+%info in first result set (they should all be the same)
 emotionList = results{1}.result.info.groupList;
 numEmotions = length(emotionList);
 figureNum = 0;
 
 myGroupResults = cell(numEmotions,1);
+mySubjSlopes = zeros(numSubjects, numEmotions);
 
 %loop through each emotion
 for emotionNum = 1:numEmotions
     emotion = emotionList{emotionNum};
-
     videoList = results{1}.result.(emotion).info.movieList;
+
     numVid = length(videoList);
 
-    %TO DO INITIALIZE SUBJECT ARRAY TO NANANANANANAN
-
-    subjCorrelation = nan(numResults,numVid);
+    subjCorrelation = nan(numSubjects,numVid);
 
     %loop through each video
+    tempSlopes = zeros(numSubjects, numVid);
     for vidNum = 1:numVid
         [~,videoBaseName,~] = fileparts(videoList{vidNum});
         duration = results{1}.result.(emotion).(videoBaseName).info.duration;
         fps = results{1}.result.(emotion).(videoBaseName).info.fps;
         numExpectedDatapoints = round(duration*fps);
 
-        Y=nan(numExpectedDatapoints,numResults);
-        for subjNum = 1:length(results)
-            %videoStruct = interpolateData(results{subjNum}.result.(emotion).(videoBaseName));
+        Y=nan(numExpectedDatapoints,numSubjects);
+        for subjNum = 1:numSubjects
             subjData = results{subjNum}.result.(emotion).(videoBaseName).data(:,3);
 
             dataLength = length(subjData);
             Y(1:dataLength,subjNum) = subjData;
         end
 
-        %%%%%%%%%%%%%%%%%%%%%%%%%%
-        % if you want to remove values that exceed the max boundary you can do it here.
-        %Change any values > 20 to 20.
-
-        %TODO: Rescale timeseries with any values > 20
-        %inds = find(Y > 20); Y(inds) = 20;
-        %meanAcrossSubjects = nanmean(Y,2);
-
-        % nanmean calculates mean while ignoring NaNs
+        %to do check for nan
         meanAcrossSubjects = nanmean(Y,2);
 
-
-        % replace with standard error of the mean. %%%%%%%%%%%%%%%%%%%%%
-        % se = std(x)/sqrt(n), where n = number of values in x
-        % std is the sample variance
-        % se is the estimate of the population variance
         standardDeviationAcrossSubjects = std(Y,0,2);
 
+        %for every subject and video, find the slope between mean and
+        %subject timeseries
+        for subjNum = 1:numSubjects
+            B = robustfit(meanAcrossSubjects,Y(:,subjNum));
+            tempSlopes(subjNum, vidNum) = B(2);
+        end
+
         figureNum = figureNum + 1;
-        % You could also just do figure(vidNum) %%%%%%%%%%%%%%%%%%%%%
         figure(figureNum);
         g = plot(meanAcrossSubjects,'-k');
         set(g,'linewidth',3)
         hold on
         plot(Y)
-
         g = plot(meanAcrossSubjects-standardDeviationAcrossSubjects,':k');
         set(g,'linewidth',2)
         g = plot(meanAcrossSubjects+standardDeviationAcrossSubjects,':k');
         set(g,'linewidth',2)
-        % alternative to strcat and fullfile: title([emotion,' video #',num2str(vidNum)]) %%%%%%%%%%%%%
         title(strcat(emotion,' video #',num2str(vidNum)))
 
         subjCorrelation(:,vidNum) = corr(Y,meanAcrossSubjects,'rows','complete');
 
-        %find correlation between subject and mean of all subject
-        %TODO: Solve the NaN problem
-        %TODO: Create a function that will cleanup the data. Do the
-        %interpolation of the timeseries, etc.
-        %for corrNum = 1:subjNum
-        %    temp=corrcoef(Y(:,corrNum),meanAcrossSubjects);
-        %end
-
-        %temp=corr(Y, meanAcrossSubjects,'rows','complete');
-
-        %Get collenations for all video
-        %average of all correlation for each emotion for each subject
-        % relationship between different correlations of different emotions
-
-        %regression analysis - techniwue to predict Y given X
-        %load the RSQ data from text file
-        %Similarity to: correlate the RSQ against correlation of the mean across subjects and the individual rating
-        %Sensitivity: correlate the RSQ  against the slope of the regression (scaling factor)
-
-        %robustfit or regress functions - basic linear equation
-
-        %bar plot of correlation of each emotion
     end
 
+    %calc mean of slope for each video
+    mySubjSlopes(:,emotionNum) = mean(tempSlopes,2);
+
     myGroupResults{emotionNum} = subjCorrelation;
-    %TODO: This should need to be 'nanmean'
-    subjectByEmotionCorr(:,emotionNum) = mean(subjCorrelation,2);
-
-    plot(1:7,mean(myGroupResults{1}))
-    xlabel('disgust')
-    ylabel('corr to the mean')
+    subjByEmotionCorrelation(:,emotionNum) = mean(subjCorrelation,2);
 
 
-
-    %subject by emotion
-
-    % for upper bounds always use a variable
-    % otherwise you will have to change your code every time you add a subject
-    % correlationTable = table(subjIDList,subjCorrelation{1:numSubj});
-
-    %videoCorrelation=subjCorrelation.';
     %correlation between subject and mean of all subjects
-    %correlationTable = table(num2str(subjIDList),subjCorrelation(:,1:numVid));
-    %     correlationTable = table(num2str(subjIDList),{subjCorrelation});
-
-    % you can convert a number into a string using num2str(x)
-    % e.g. for i=1:9
-    % mystring = ['00' num2str(i)];
-    % end
-    % for i = 10:99
-    % mystring = ['0' num2str(i)];
-    % end
-    %
-    % bonus points if you can combine these two loops into one
-    %     correlationTable.Properties.VariableNames = ["subjID","Video 001","Video 002","Video 003","Video 004","Video 005","Video 006","Video 007"];
-    %     fprintf("Correlation on subject ratings between %s videos:\n",emotion);
+    %     correlationTable = table(subjIDList,subjCorrelation(1:7);
+    %     correlationTable.Properties.VariableNames = ["subjID","001","002","003","004","005","006","007"];
+    %     fprintf("%s\n",emotion);
     %     disp(correlationTable);
 
     %plot correlation between subject and mean of all subjects per emotion
@@ -163,80 +110,75 @@ for emotionNum = 1:numEmotions
     %line of best fit using robustfit, final relationship = final
     %predicted - slope (as corr increases, RS decreases)
 
+
 end
 
-figure
-% emoCorrForRegression = zeros(factorial(numEmotions-1)*numResults,2);
+%figure for all emotions
+figure;
 emoCorrForRegression = [];
-for emotionNumX = 1:numEmotions-1
+
+for emotionNumX = 1:numEmotions-1;
     for emotionNumY = emotionNumX+1:numEmotions
-        temp = [subjectByEmotionCorr(:,emotionNumX),subjectByEmotionCorr(:,emotionNumY)];
+        temp=[subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY)];
         emoCorrForRegression = [emoCorrForRegression;temp];
-        plot(subjectByEmotionCorr(:,emotionNumX),subjectByEmotionCorr(:,emotionNumY),'ro')
-        xlabel('correlation');
-        ylabel('correlation');
-        title('All emotions')
+        plot(subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY),'ro');
         hold on;
     end
 end
+
 [B, stats] = robustfit(emoCorrForRegression(:,1),emoCorrForRegression(:,2));
-%stats.p is P-value. Anything less than .05 is not by chance (e.g.
-%statistically significant)
-%y=ax+b
-plot(emoCorrForRegression(:,1), emoCorrForRegression(:,1)*B(2)+B(1));
-disp(['P-value for all datapoints: ', num2str(stats.p(2))]);
+%y = ax + b
+plot(emoCorrForRegression(:,1),emoCorrForRegression(:,1)*B(2)+B(1));
+xlabel('correlation');
+ylabel('correlation');
+title(['all emotions (P-value: ',num2str(stats.p(2)),')']);
+disp(['P-value for All Data Points: ',num2str(stats.p(2))]);
 
+%figures for each emotion
+emoCorrForRegression = [];
 
-% emoCorrForRegression = zeros(factorial(numEmotions-1)*numResults,2);
-for emotionNumX = 1:numEmotions-1
-    emoCorrForRegression = [];
+for emotionNumX = 1:numEmotions-1;
     for emotionNumY = emotionNumX+1:numEmotions
-        figure
-
-%         temp = [subjectByEmotionCorr(:,emotionNumX),subjectByEmotionCorr(:,emotionNumY)];
-%         emoCorrForRegression = [emoCorrForRegression;temp];
-        emoCorrForRegression = [subjectByEmotionCorr(:,emotionNumX),subjectByEmotionCorr(:,emotionNumY)];
-        plot(subjectByEmotionCorr(:,emotionNumX),subjectByEmotionCorr(:,emotionNumY),'ro')
-        xlabel([emotionList{emotionNumX}, ' correlation']);
-        ylabel([emotionList{emotionNumY}, ' correlation']);
-        title([emotionList{emotionNumX}, ' vs. ', emotionList{emotionNumY} ' correlation (P-value: ', num2str(stats.p(2)), ')'])
+        figure;
+        emoCorrForRegression = [subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY)];
+        plot(subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY),'ro');
+        xlabel([emotionList{emotionNumX},' correlation']);
+        ylabel([emotionList{emotionNumY},' correlation']);
+        title([emotionList{emotionNumX},' vs. ', emotionList{emotionNumY}, ' correlation (P-value: ',num2str(stats.p(2)),')']);
+        disp(['P-value for ',emotionList{emotionNumX},' vs. ', emotionList{emotionNumY},': ',num2str(stats.p(2))]);
         hold on;
-
-    [B, stats] = robustfit(emoCorrForRegression(:,1),emoCorrForRegression(:,2));
-    %stats.p is P-value. Anything less than .05 is not by chance (e.g.
-    %statistically significant)
-    %y=ax+b
-    plot(emoCorrForRegression(:,1), emoCorrForRegression(:,1)*B(2)+B(1));
-    disp(['P-value for all datapoints: ', num2str(stats.p(2))]);
+        [B, stats] = robustfit(emoCorrForRegression(:,1),emoCorrForRegression(:,2));
+        %y = ax + b
+        plot(emoCorrForRegression(:,1),emoCorrForRegression(:,1)*B(2)+B(1));
+        disp(['regression slope: ',num2str(B(2))]);
+        disp(['regression int: ',num2str(B(1))]);
     end
 end
 
+rsScores = importdata('results/rs-scores.csv')
 
-
-
-
-function videoStruct = interpolateData(videoStruct)
-
-secondsPerFrame=1/videoStruct.info.fps
-numFrames=round(videoStruct.info.duration/secondsPerFrame)
-xq = (videoStruct.data(1,1):secondsPerFrame:videoStruct.data(1,1)+(secondsPerFrame*numFrames))
-reshape(xq,[],1)
-
-fprintf(videoStruct)
-
+for emotionNum = 1:numEmotions
+    %rsCorrForRegression = subjByEmotionCorrelation(:,emotionNum),rsScores.data(:,2);
+    figure;
+    plot(subjByEmotionCorrelation(:,emotionNum), ...
+        rsScores.data(:,2),'o');
+    hold on;
+    [B, stats] = robustfit(subjByEmotionCorrelation(:,emotionNum),rsScores.data(:,2));
+    plot(subjByEmotionCorrelation(:,emotionNum),subjByEmotionCorrelation(:,emotionNum)*B(2)+B(1),'ro-');
+    xlabel([emotionList{emotionNum},' correlation']);
+    ylabel('RS Score');
+    title([emotionList{emotionNum},' vs. RS Score (P-value: ',num2str(stats.p(2)),')']);
 end
 
+%scaling factor of each subject plotted against RS
+%mySubjSlopes is the scaling factor to get the values for each subject
+%same analysis as the correlation of each subject
+%correltate mySubjSlopes with rsScores
+% mean across subject for a particular emotion
+% Regression (robustfit):
+%X - Independent variable
+%Y - Dependent variable
 
-% y1 = smoothdata(rand(1069,1),'gaussian', 100);
-% y2 = smoothdata(rand(873,1),'gaussian',100);
-% y3 = smoothdata(rand(500,1),'gaussian',100);
-% y4 = smoothdata(rand(1000,1),'gaussian',100);
-% Y=nan(4,1000);
-% Y(1,:) = interp1(y1,linspace(1,numel(y1),1000));
-% Y(2,:) = interp1(y2,linspace(1,numel(y2),1000));
-% Y(3,:) = interp1(y3,linspace(1,numel(y3),1000));
-% Y(4,:) = interp1(y4,linspace(1,numel(y4),1000));
-% h=plot(linspace(0,100,1000),Y,'Color',[.7 .7 .7]);
-% hold on
-% hh=plot(linspace(0,100,1000),mean(Y),'k-','linewidth',2);
-% legend([h(1) hh],'Individual','Average')
+%atanh, then convert back using atan
+
+
