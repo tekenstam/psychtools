329bb371fa333c15beb3473430f4a3826b2a9373
Todd Ekenstam <tekenstam@gmail.com>
2023-03-13 09:45:45 -0700

    Minor formatting/whitespace changes

--- MovieRating/analyzeData.m
+++ MovieRating/analyzeData.m
@@ -1,24 +1,25 @@
 %clear all variables and close all open diagrams
-clear all;
+clear all;   %#ok - We want to clear all variables before running
 close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %load results from result files
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if exist('/Users/annikaekenstam/Dropbox/Stimuli/results/','dir')
+if exist('/Users/annikaekenstam/Dropbox/Stimuli/results/', 'dir')
     resultsFolder = '/Users/annikaekenstam/Dropbox/Stimuli/results/';
-elseif exist('/Users/corelabuser/Dropbox/Stimuli/results/','dir')
+elseif exist('/Users/corelabuser/Dropbox/Stimuli/results/', 'dir')
     resultsFolder = '/Users/corelabuser/Dropbox/Stimuli/results/';
 else
     resultsFolder = 'results/';
 end
 
-resultList = dir([resultsFolder,'*.mat']);
+resultList = dir([resultsFolder, '*.mat']);
 numSubjects = length(resultList);
-subjIDList = zeros(numSubjects,1);
+results = cell(numSubjects);
+subjIDList = zeros(numSubjects, 1);
 
-for resultNum = 1:numSubjects;
-    results{resultNum}=load([resultsFolder,resultList(resultNum).name]);
+for resultNum = 1:numSubjects
+    results{resultNum} = load([resultsFolder, resultList(resultNum).name]);
     subjIDList(resultNum,1) = results{resultNum}.result.subID;
 end
 
@@ -29,7 +30,7 @@ end
 emotionList = results{1}.result.info.groupList;
 numEmotions = length(emotionList);
 
-myGroupResults = cell(numEmotions,1);
+myGroupResults = cell(numEmotions, 1);
 mySubjSlopes = zeros(numSubjects, numEmotions);
 
 
@@ -38,13 +39,14 @@ mySubjSlopes = zeros(numSubjects, numEmotions);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %loop through each emotion
+subjByEmotionCorrelation = nan(numSubjects, numEmotions);
 for emotionNum = 1:numEmotions
     emotion = emotionList{emotionNum};
     videoList = results{1}.result.(emotion).info.movieList;
 
     numVid = length(videoList);
 
-    subjCorrelation = nan(numSubjects,numVid);
+    subjCorrelation = nan(numSubjects, numVid);
 
     %loop through each video
     tempSlopes = zeros(numSubjects, numVid);
@@ -52,9 +54,9 @@ for emotionNum = 1:numEmotions
         [~,videoBaseName,~] = fileparts(videoList{vidNum});
         duration = results{1}.result.(emotion).(videoBaseName).info.duration;
         fps = results{1}.result.(emotion).(videoBaseName).info.fps;
-        numExpectedDatapoints = round(duration*fps);
+        numExpectedDatapoints = round(duration * fps);
 
-        videoResults=nan(numExpectedDatapoints,numSubjects);
+        videoResults=nan(numExpectedDatapoints, numSubjects);
         for subjNum = 1:numSubjects
             subjData = results{subjNum}.result.(emotion).(videoBaseName).data(:,3);
 
@@ -62,10 +64,9 @@ for emotionNum = 1:numEmotions
             videoResults(1:dataLength,subjNum) = subjData;
         end
 
-        %TODO: check for nan
-        meanAcrossSubjects = nanmean(videoResults,2);
+        meanAcrossSubjects = mean(videoResults, 2);
 
-        standardDeviationAcrossSubjects = std(videoResults,0,2);
+        standardDeviationAcrossSubjects = std(videoResults, 0, 2);
 
         %for each subject and video, find the slope between mean and
         %subject timeseries
@@ -74,29 +75,30 @@ for emotionNum = 1:numEmotions
             tempSlopes(subjNum, vidNum) = B(2);
         end
 
+
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %plot mean and +/- standard deviation across subjects
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         figure;
-        g = plot(meanAcrossSubjects,'-k');
-        set(g,'linewidth',3)
+        g = plot(meanAcrossSubjects, '-k');
+        set(g, 'linewidth', 3)
         hold on
         plot(videoResults)
-        g = plot(meanAcrossSubjects-standardDeviationAcrossSubjects,':k');
-        set(g,'linewidth',2)
-        g = plot(meanAcrossSubjects+standardDeviationAcrossSubjects,':k');
-        set(g,'linewidth',2)
-        title(strcat(emotion,' video #',num2str(vidNum)))
+        g = plot(meanAcrossSubjects-standardDeviationAcrossSubjects, ':k');
+        set(g, 'linewidth', 2)
+        g = plot(meanAcrossSubjects+standardDeviationAcrossSubjects, ':k');
+        set(g, 'linewidth', 2)
+        title([emotion, ' video #', num2str(vidNum)]);
 
         subjCorrelation(:,vidNum) = corr(videoResults, meanAcrossSubjects, 'rows', 'complete');
 
     end
 
     %calculate mean of slope for each video
-    mySubjSlopes(:,emotionNum) = mean(tempSlopes,2);
+    mySubjSlopes(:,emotionNum) = mean(tempSlopes, 2);
 
     myGroupResults{emotionNum} = subjCorrelation;
-    subjByEmotionCorrelation(:,emotionNum) = mean(subjCorrelation,2);
+    subjByEmotionCorrelation(:,emotionNum) = mean(subjCorrelation, 2);
 
 end
 
@@ -106,46 +108,47 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 figure;
-emoCorrForRegression = [];
-
-for emotionNumX = 1:numEmotions-1;
+emoCorrForRegression = nan(numSubjects * factorial(numEmotions - 1), 2);
+for emotionNumX = 1:numEmotions-1
     for emotionNumY = emotionNumX+1:numEmotions
-        temp=[subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY)];
-        emoCorrForRegression = [emoCorrForRegression;temp];
-        plot(subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY),'ro');
+        temp=[subjByEmotionCorrelation(:,emotionNumX), ...
+            subjByEmotionCorrelation(:,emotionNumY)];
+        emoCorrForRegression = [emoCorrForRegression;temp]; %#ok - Collect all correlations
+        plot(subjByEmotionCorrelation(:,emotionNumX), ...
+            subjByEmotionCorrelation(:,emotionNumY), 'ro');
         hold on;
     end
 end
 
-[B, stats] = robustfit(emoCorrForRegression(:,1),emoCorrForRegression(:,2));
+[B, stats] = robustfit(emoCorrForRegression(:,1), emoCorrForRegression(:,2));
 %y = ax + b
-plot(emoCorrForRegression(:,1),emoCorrForRegression(:,1)*B(2)+B(1));
-xlabel('correlation');
-ylabel('correlation');
-title(['all emotions (P-value: ',num2str(stats.p(2)),')']);
-disp(['P-value for All Data Points: ',num2str(stats.p(2))]);
+plot(emoCorrForRegression(:,1), emoCorrForRegression(:,1) * B(2) + B(1));
+xlabel('Correlation');
+ylabel('Correlation');
+title(['All emotions (P-value: ', num2str(stats.p(2)), ')']);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %subject correlation between each emotion
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-emoCorrForRegression = [];
-for emotionNumX = 1:numEmotions-1;
+emoCorrForRegression = nan(numSubjects, 2);
+for emotionNumX = 1:numEmotions-1
     for emotionNumY = emotionNumX+1:numEmotions
         figure;
-        emoCorrForRegression = [subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY)];
-        plot(subjByEmotionCorrelation(:,emotionNumX),subjByEmotionCorrelation(:,emotionNumY),'ro');
-        xlabel([emotionList{emotionNumX},' correlation']);
-        ylabel([emotionList{emotionNumY},' correlation']);
-        title([emotionList{emotionNumX},' vs. ', emotionList{emotionNumY}, ' correlation (P-value: ',num2str(stats.p(2)),')']);
-        disp(['P-value for ',emotionList{emotionNumX},' vs. ', emotionList{emotionNumY},': ',num2str(stats.p(2))]);
+        emoCorrForRegression = [subjByEmotionCorrelation(:,emotionNumX), ...
+            subjByEmotionCorrelation(:,emotionNumY)];
+        plot(subjByEmotionCorrelation(:,emotionNumX), ...
+            subjByEmotionCorrelation(:,emotionNumY), 'ro');
+        xlabel([emotionList{emotionNumX}, ' correlation']);
+        ylabel([emotionList{emotionNumY}, ' correlation']);
+        title([emotionList{emotionNumX}, ' vs. ', emotionList{emotionNumY}, ...
+            ' correlation (P-value: ', num2str(stats.p(2)), ')']);
         hold on;
-        [B, stats] = robustfit(emoCorrForRegression(:,1),emoCorrForRegression(:,2));
+        [B, stats] = robustfit(emoCorrForRegression(:,1), ...
+            emoCorrForRegression(:,2));
         %y = ax + b
-        plot(emoCorrForRegression(:,1),emoCorrForRegression(:,1)*B(2)+B(1));
-        disp(['regression slope: ',num2str(B(2))]);
-        disp(['regression int: ',num2str(B(1))]);
+        plot(emoCorrForRegression(:,1), emoCorrForRegression(:,1) * B(2) + B(1));
     end
 end
 
@@ -154,17 +157,21 @@ end
 %subject correlation vs RS score for each emotion
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-rsScores = importdata('results/rs-scores.csv')
+rsScores = importdata('results/rs-scores.csv');
 
 for emotionNum = 1:numEmotions
     figure;
-    plot(subjByEmotionCorrelation(:,emotionNum), rsScores.data(:,2),'o');
+    plot(subjByEmotionCorrelation(:,emotionNum), rsScores.data(:,2), 'o');
     hold on;
-    [B, stats] = robustfit(subjByEmotionCorrelation(:,emotionNum),rsScores.data(:,2));
-    plot(subjByEmotionCorrelation(:,emotionNum),subjByEmotionCorrelation(:,emotionNum)*B(2)+B(1),'r-');
+    [B, stats] = robustfit(subjByEmotionCorrelation(:,emotionNum), ...
+        rsScores.data(:,2));
+    %y = ax + b
+    plot(subjByEmotionCorrelation(:,emotionNum), ...
+        subjByEmotionCorrelation(:,emotionNum) * B(2) + B(1), 'r-');
     xlabel([emotionList{emotionNum},' correlation']);
-    ylabel('RS Score');
-    title([emotionList{emotionNum},' vs. RS Score (P-value: ',num2str(stats.p(2)),')']);
+    ylabel('RS score');
+    title([emotionList{emotionNum},' vs. RS score (P-value: ', ...
+        num2str(stats.p(2)), ')']);
 end
 
 
@@ -176,13 +183,15 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 figure;
-plot(mean(mySubjSlopes,2), rsScores.data(:,2), 'o');
+plot(mean(mySubjSlopes, 2), rsScores.data(:,2), 'o');
 hold on;
-[B, stats] = robustfit(mean(mySubjSlopes,2),rsScores.data(:,2));
-plot(mean(mySubjSlopes,2),mean(mySubjSlopes,2)*B(2)+B(1),'r-');
+[B, stats] = robustfit(mean(mySubjSlopes, 2), rsScores.data(:,2));
+%y = ax + b
+plot(mean(mySubjSlopes, 2), mean(mySubjSlopes, 2) * B(2) + B(1), 'r-');
 xlabel('Mean slope of correlation for all emotions');
-ylabel('RS Score');
-title(['Mean slope of all emotions vs. RS Score (P-value: ',num2str(stats.p(2)),')']);
+ylabel('RS score');
+title(['Mean slope of all emotions vs. RS score (P-value: ', ...
+    num2str(stats.p(2)), ')']);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -195,12 +204,13 @@ for emotionNum = 1:numEmotions
     figure;
     plot(mySubjSlopes(:,emotionNum), rsScores.data(:,2), 'o');
     hold on;
-    [B, stats] = robustfit(mySubjSlopes(:,emotionNum),rsScores.data(:,2));
-    plot(mySubjSlopes(:,emotionNum),mySubjSlopes(:,emotionNum)*B(2)+B(1),'r-');
+    [B, stats] = robustfit(mySubjSlopes(:,emotionNum), rsScores.data(:,2));
+    %y = ax + b
+    plot(mySubjSlopes(:,emotionNum), mySubjSlopes(:,emotionNum) * B(2) + B(1), 'r-');
     xlabel(['Slope of ', emotionList{emotionNum}, ' correlation']);
-    ylabel('RS Score');
+    ylabel('RS score');
     title(['Slope of ', emotionList{emotionNum}, ...
-        ' correlation vs. RS Score (P-value: ', num2str(stats.p(2)), ')']);
+        ' correlation vs. RS score (P-value: ', num2str(stats.p(2)), ')']);
 
 end
 
